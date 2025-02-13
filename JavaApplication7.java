c170c90257558fba3c99c52f9ba83e29f4fff3b3
ameli4sr <amelsr0409@gmail.com>
2025-02-13 15:37:32 +0700

    WS SI MINGGU 2\ACARA 5 DAN 6 NO1

---
+++ JavaApplication7.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication7;
+
+
+public class JavaApplication7 {
+
+    public static void main(String[] args) {
+       String fName = "Lisa";
+       String lName = "Pambolo";
+       int stuId = 123456789;
+       String stuStatus = "Active";
+     
+       
+       System.out.println("Student Name :" + fName +"" + lName);
+       System.out.println("Student ID : " + stuId);
+       System.out.println("Student Status :" + stuStatus);
+    }
+    
+}
